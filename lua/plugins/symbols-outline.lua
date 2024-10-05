3e4405dec513305310ebecc9a11324874a9d5f81
Abdul Shabbir <abdulqshabbir@gmail.com>
2024-10-05 14:22:32 +0800

    feat: :sparkles: add symbols outline plugin

---
+++ lua/plugins/symbols-outline.lua
@@ -0,0 +1,21 @@
+return {
+  "simrat39/symbols-outline.nvim",
+  opts = {
+    keymaps = { -- These keymaps can be a string or a table for multiple keys
+      close = { "<Esc>", "q" },
+      goto_location = "<Cr>",
+      focus_location = "o",
+      toggle_preview = "K",
+      rename_symbol = "r",
+      code_actions = "a",
+      fold = "zc",
+      unfold = "zo",
+      fold_all = "W",
+      unfold_all = "E",
+      fold_reset = "R",
+    },
+  },
+  keys = {
+    { "<leader>so", "<cmd>SymbolsOutline<CR>", { desc = "Open symbols map" } },
+  },
+}
